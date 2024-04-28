890fad714e2a6da031f179104d722915caf45ba8
ren <r999b66k222@outlook.jp>
2024-04-28 18:07:42 +0900

    refactor: valueObjectをDDDへリファクタ

--- laravel-project/app/Models/Memo.php
+++ laravel-project/app/Models/Memo.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Http\Request;
+use App\Domain\Memo\Id;
+use App\Domain\Memo\Title;
+use App\Domain\Memo\Content;
 
 class Memo extends Model
 {
@@ -20,6 +22,35 @@ class Memo extends Model
         'title',
         'content'
     ];
+
+    // id
+    public function setIdAttribute($value)
+    {
+        $this->attributes['id'] = new Id($value);
+    }
+    public function getIdAttribute($value)
+    {
+        return $value instanceof Id ? $value->value() : $value;
+    }
+    // title
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = new Title($value);
+    }
+    public function getTitleAttribute($value)
+    {
+        return $value instanceof Title ? $value->value() : $value;
+    }
+    // content
+    public function setContentAttribute($value)
+    {
+        $this->attributes['content'] = new Content($value);
+    }
+    public function getContentAttribute($value)
+    {
+        return $value instanceof Content ? $value->value() : $value;
+    }
+
 
     public function scopeSearch($query, $keyword)
     {
