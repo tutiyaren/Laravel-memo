07e2d3aff5ee40cf1f2941bba1b04fb1e6493cfa
ren <r999b66k222@outlook.jp>
2024-05-10 06:54:43 +0900

    refactor: モデルにvalueObject追加

--- laravel-project/app/Models/Category.php
+++ laravel-project/app/Models/Category.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Domain\Category\Id;
+use App\Domain\Category\Name;
 
 class Category extends Model
 {
@@ -16,5 +18,24 @@ class Category extends Model
     public function memo_categories()
     {
         return $this->hasMany(Memo_Category::class, 'category_id');
+    }
+
+    // id
+    public function setIdAttribute($value)
+    {
+        $this->attributes['id'] = new Id($value);
+    }
+    public function getIdAttribute($value)
+    {
+        return $value instanceof Id ? $value->value() : $value;
+    }
+    // name
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = new Name($value);
+    }
+    public function getNameAttribute($value)
+    {
+        return $value instanceof Name ? $value->value() : $value;
     }
 }
